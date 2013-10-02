f830af4ffef204ac4ec1dd866be9a4098db7e4b5
Alvaro H. M. Aliaga <alvarohenry@gmail.com>
2013-10-02 09:59:55 -0500

    Business: JSON format, using Gson.

--- business/src/main/java/org/zagile/quiz/business/DataRetriever.java
+++ business/src/main/java/org/zagile/quiz/business/DataRetriever.java
@@ -1,11 +1,15 @@
 package org.zagile.quiz.business;
 
+import java.util.HashMap;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
+
+import com.google.gson.Gson;
 
 @Service("dataRetriever")
 public class DataRetriever implements IDataRetriever {
@@ -22,25 +26,29 @@ public class DataRetriever implements IDataRetriever {
 			conf = new Configuration();
 			conf.set("fs.default.name", "hdfs://localhost:54310");
 			dfs = FileSystem.get(conf);
-			//Path path = new Path("/");
-			//FileStatus[] listStatus = fs.listStatus(path);
-			//logger.info("=======>" + listStatus.length);
+			
 			Path path = new Path(dfs.getWorkingDirectory().toString());
-			
 			FileStatus[] filesFirstLevel = dfs.listStatus(path); // first level
-			
+			HashMap<String, String> result = new HashMap<String, String>();
 			for(FileStatus fileFL : filesFirstLevel){
-				System.out.println(fileFL.getPath().getName());
 				if (fileFL.isDir()) { // second level
+					logger.info("First Level. Reading a directory : " + fileFL.getPath().getName());
 					Path pathSL = new Path(fileFL.getPath().toString());
 					FileStatus[] filesSecondLevel = dfs.listStatus(pathSL);
+					String value = "";
 					for(FileStatus fileSL : filesSecondLevel){
-						System.out.println("\t" + fileSL.getPath().getName());
+						logger.info("Second Level. Reading ... : " + fileSL.getPath().getName());
+						value += fileSL.getPath().getName() + ",";
 					}
+					result.put(fileFL.getPath().getName(), value);
+				} else {
+					logger.info("First Level. Reading a file : " + fileFL.getPath().getName());
+					result.put(fileFL.getPath().getName(), "");
 				}
 			}
 			dfs.close();
-			return "JSONFORMAT";
+			logger.info("Converting the result to JSON format.");
+			return (new Gson()).toJson(result);
 		} catch (Exception e) {
 			throw new RuntimeException(e.getMessage());
 		}
